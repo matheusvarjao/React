26c77dec7334fe8e98f08ceff947fb641e56ed48
matheusvarjao <133286026+matheusvarjao@users.noreply.github.com>
2024-04-03 22:09:16 -0300

    Add files via upload

---
+++ src/app/api/country/route.js
@@ -0,0 +1,21 @@
+import {nextResponde, NextResponse} from "next/server";
+import data from "./../../../../data.json";
+
+export async function GET(){
+    try{
+        const response = data.map((country)=> {
+            return {
+                name:country.name,
+                population:country.population,
+                region:country.region,
+                capital:country.capital,
+                flag:country.flags.svg,
+                code:country.numericCode,
+            };
+        });
+        return NextResponse.json(response,{status:200});
+    } catch (error) {
+    console.log(error);
+    return NextResponse.json({msg:"Operation Failed!"},{status:500})
+    }
+}
